ea7f3011af9b8bb310fcd38522efcbe9266f36f4
99Rafick <rafraf9727@gmail.com>
2023-04-05 12:45:37 +0100

    theme

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,13 @@ Selector = (parametre) => {
 }
 
 
+SelectorAll = (parametre) => {
+    if(typeof(parametre) != 'string'){
+        return false
+    }
+    return document.querySelectorAll(parametre);
+}
+
 AddClicked = (parametre) => {
     parametre.addEventListener('click', (e) => {
         parametre.classList.toggle('clicked');
@@ -16,6 +23,30 @@ AddClicked = (parametre) => {
 
 let btnTheme = Selector('.btn-theme')
 let filter = Selector('.filter');
+let navLinks = SelectorAll('.aside-nav ul li')
+let glisse = Selector('.glisse')
+let body = Selector('body')
 
-AddClicked(btnTheme);
+
 AddClicked(filter);
+
+btnTheme.addEventListener('click', (e) => {
+    btnTheme.classList.toggle('clicked');
+    body.classList.toggle('dark')
+})
+
+
+
+navLinks.forEach((element, k) => {
+    element.addEventListener('click', (e) => {
+        let step = k
+        glisse.style.transform = `translateY(${(step * 60)}px)`
+        console.log(step);
+        element.classList.add('active');
+        let parent = element.parentNode.querySelector('.active')
+        parent.classList.remove('active');
+        
+    }) 
+});
+
+
